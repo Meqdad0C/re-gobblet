9620e71479728b1403c96528c0367a12ade4dbac
Meqdad <Meqdad_@outlook.com>
2023-12-15 04:07:36 +0200

    Add react-dnd and react-draggable dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -19,47 +19,45 @@ enum Size {
  * Size is either small, medium, or large
  */
 const Piece = ({ color, size }: { color: Player; size: Size }) => {
+
   return (
-    <div
-      className={cn(
-        ['absolute rounded-full border-2 border-black'],
-        {
-          'bg-red-500': color === Player.Red,
-          'bg-blue-500': color === Player.Blue,
-        },
-        {
-          'h-14 w-14': size === Size.Small,
-          'h-20 w-20': size === Size.Medium,
-          'h-24 w-24': size === Size.Large,
-          'h-28 w-28': size === Size.XLarge,
-        },
-        {
-          'hover:bg-red-400': color === Player.Red,
-          'hover:bg-blue-400': color === Player.Blue,
-        },
-        {
-          'hover:h-16 hover:w-16': size === Size.Small,
-          'hover:h-24 hover:w-24': size === Size.Medium,
-          'hover:h-28 hover:w-28': size === Size.Large,
-          'hover:h-32 hover:w-32': size === Size.XLarge,
-        },
-        {
-          'z-10': size === Size.Small,
-          'z-20': size === Size.Medium,
-          'z-30': size === Size.Large,
-          'z-40': size === Size.XLarge,
-        },
-        {
-          'text-sm': size === Size.Small,
-          'text-base': size === Size.Medium,
-          'text-lg': size === Size.Large,
-          'text-xl': size === Size.XLarge,
-        },
-      )}
-      onDrag={() => {
-        console.log('drag')
-      }}
-    />
+      <div
+        className={cn(
+          ['absolute rounded-full border-2 border-black'],
+          {
+            'bg-red-500': color === Player.Red,
+            'bg-blue-500': color === Player.Blue,
+          },
+          {
+            'h-14 w-14': size === Size.Small,
+            'h-20 w-20': size === Size.Medium,
+            'h-24 w-24': size === Size.Large,
+            'h-[6.5rem] w-[6.5rem]': size === Size.XLarge,
+          },
+          {
+            'hover:bg-red-400': color === Player.Red,
+            'hover:bg-blue-400': color === Player.Blue,
+          },
+          {
+            'hover:h-16 hover:w-16': size === Size.Small,
+            'hover:h-20 hover:w-20': size === Size.Medium,
+            'hover:h-24 hover:w-24': size === Size.Large,
+            'hover:h-[7rem] hover:w-[7rem]': size === Size.XLarge,
+          },
+          {
+            'z-10': size === Size.Small,
+            'z-20': size === Size.Medium,
+            'z-30': size === Size.Large,
+            'z-40': size === Size.XLarge,
+          },
+          {
+            'text-sm': size === Size.Small,
+            'text-base': size === Size.Medium,
+            'text-lg': size === Size.Large,
+            'text-xl': size === Size.XLarge,
+          },
+        )}
+      />
   )
 }
 
@@ -67,7 +65,7 @@ const Inventory = ({ player }: { player: Player }) => {
   const stack = [Size.XLarge, Size.Large, Size.Medium, Size.Small]
 
   return (
-    <div className='grid h-40 w-full grid-cols-3 rounded-2xl border-2 border-black'>
+    <div className='grid h-40 w-full grid-cols-3 rounded-2xl border-2 border-black dark:border-white'>
       {[0, 1, 2].map((i) => (
         <div key={i} className='grid items-center justify-items-center'>
           {stack.map((size, idx) => (
@@ -81,7 +79,7 @@ const Inventory = ({ player }: { player: Player }) => {
 
 const Board = () => {
   return (
-    <div className='flex flex-col gap-2'>
+    <div className='board relative flex flex-col gap-2'>
       {[0, 1, 2, 3].map((row) => (
         <Row key={row} row={row} />
       ))}
@@ -136,12 +134,14 @@ const Game = () => {
 
 export default function App() {
   return (
-    <main className='container flex min-h-screen flex-col items-center justify-center gap-2'>
-      <div className='fixed right-5 top-5'>
-        <ModeToggle />
-      </div>
-      <h1 className='text-center text-5xl font-bold'>Gobblet</h1>
-      <Game />
-    </main>
+    <>
+      <main className='container flex min-h-screen flex-col items-center justify-center gap-2'>
+        <div className='fixed right-5 top-5'>
+          <ModeToggle />
+        </div>
+        <h1 className='text-center text-5xl font-bold'>Gobblet</h1>
+        <Game />
+      </main>
+    </>
   )
 }
