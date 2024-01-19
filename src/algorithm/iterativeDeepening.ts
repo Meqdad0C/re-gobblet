7d13bf3f7cfab59f93826672d474b77e9cafa25f
raghad moustafa <raghda.mustafa.anwar@gmail.com>
2024-01-19 04:15:42 +0200

    iterative

---
+++ src/algorithm/iterativeDeepening.ts
@@ -0,0 +1,52 @@
+import { Player, GameState } from '@/types'
+import { minimax_with_pruning } from './alphaBeta'
+// const INFINITY: number = Number.POSITIVE_INFINITY
+
+export function iterativeDeepeningMinimax(
+  gameState: GameState,
+  maxDepth: number,
+  isMaximizingPlayer: boolean,
+  turn: Player,
+  timeLimit: number,
+) {
+  let bestMove = null
+  const startTime = Date.now()
+  let bestScore = isMaximizingPlayer ? -Infinity : Infinity
+  let totalRecursionCount = 0
+
+  for (let depth = 1; depth <= maxDepth; depth++) {
+    const { score, move, recursionCount } = minimax_with_pruning(
+      gameState,
+      depth,
+      -Infinity,
+      Infinity,
+      isMaximizingPlayer,
+      turn,
+      0,
+      startTime,
+      timeLimit,
+    )
+
+    totalRecursionCount += recursionCount
+
+    //if the move come from the break then return the last best move
+    if (score === Infinity || score === -Infinity) {
+      return {
+        score: bestScore,
+        move: bestMove,
+        recursionCount: totalRecursionCount,
+      }
+    }
+
+    if (isMaximizingPlayer ? score > bestScore : score < bestScore) {
+      bestScore = score
+      bestMove = move
+    }
+  }
+
+  return {
+    score: bestScore,
+    move: bestMove,
+    recursionCount: totalRecursionCount,
+  }
+}
